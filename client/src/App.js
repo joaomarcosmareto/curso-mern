395355ca0104862806f99f3ce16c0a80dc6cc770
João Marcos Mareto Calado <joaomarcosmareto@gmail.com>
2021-11-13 13:24:15 -0300

    1. criacao da tela pública inicial e instalação do material-ui do reactjs.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+import Inicio from './pages/cliente/painel';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <h1>CURSO BÁSICO DE NODEJS COM REACTJS</h1>
-      </header>
+      </header> */}
+      <Inicio />
     </div>
   );
 }
